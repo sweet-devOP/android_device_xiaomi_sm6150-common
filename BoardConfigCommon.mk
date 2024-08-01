7058e6151aaadcd8dafbb915e7576c350cc8f254
Pranav Vashi <neobuddy89@gmail.com>
2024-08-01 22:47:56 +0530

    sm6150-common: Enable memcg on boot
    Change-Id: I39707749b8df4505488e0e59667445ba1dd99790
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: sweet-fnaticOP <orcalesyuuno@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,7 +79,7 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
 
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
-BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
+BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += loop.max_part=7
