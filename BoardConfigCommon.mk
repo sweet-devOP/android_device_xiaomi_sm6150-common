2676b6c8196209b0418798ee0d50cecbe0a39db5
donjohanliebert <donjohanliebert@gmail.com>
2025-01-24 14:07:19 +0800

    sm6150-common: declare missing FCM entries
    - generated using https://gist.github.com/SebaUbuntu/ec053a00a9988eaea091fb1cc1f19324
    
    The following HALs in device manifest are not declared in FCM <= level 5:
      android.hardware.audio.effect@7.0::IEffectsFactory/default
      android.hardware.audio@7.0::IDevicesFactory/default
      android.hardware.biometrics.fingerprint.IFingerprint/default (@4)
      android.hardware.bluetooth.audio.IBluetoothAudioProviderFactory/default (@4)
      android.hardware.drm.IDrmFactory/clearkey (@1)
      android.hardware.health.IHealth/default (@3)
      android.hardware.ir.IConsumerIr/default (@1)
      android.hardware.memtrack.IMemtrack/default (@1)
      android.hardware.power.IPower/default (@5)
      android.hardware.thermal.IThermal/default (@1)
      android.hardware.usb@1.3::IUsb/default
      android.hardware.usb.gadget.IUsbGadget/default (@1)
      android.hardware.vibrator.IVibrator/default (@2)
      android.hardware.wifi.IWifi/default (@2)
      android.hardware.wifi.hostapd.IHostapd/default (@2)
      android.hardware.wifi.supplicant.ISupplicant/default (@3)
    COMPATIBLE
    
    Change-Id: I23b72e8158379c9306675fd06ed8aa3aacd53470
    Signed-off-by: donjohanliebert <donjohanliebert@gmail.com>
    Signed-off-by: sweet-fnaticOP <orcalesyuuno@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -60,6 +60,7 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/configs/hidl/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
